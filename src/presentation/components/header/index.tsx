e71e494058cf1d9a01fc7999ed946a2e9cb2c538
sleduardo20 <sl.eduardo20@gmail.com>
2023-03-12 09:40:00 -0300

    🏭 create header component

--- src/presentation/components/header/index.tsx
+++ src/presentation/components/header/index.tsx
@@ -1,10 +1,14 @@
-import { Container } from './styles';
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 import Logo from '../../assets/logo.svg';
 
 export const Header = () => {
   return (
-    <Container>
-      <img src={Logo} alt="" />
-    </Container>
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={Logo} alt="" />
+
+        <NewTransactionButton>Nova transacao</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
   );
 };
