291a0f8b4e6498ac804cb7b69d38386b8219999f
sleduardo20 <sl.eduardo20@gmail.com>
2023-03-15 22:14:15 -0300

    ✨ create hook useTransactions

---
+++ src/presentation/hooks/useTransactions.tsx
@@ -0,0 +1,40 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { transactionService } from '../../application';
+import { Transaction } from '../../model/transation';
+
+interface TransactionContextType {
+  transactions: Transaction[];
+}
+
+const TransactionContext = createContext({} as TransactionContextType);
+
+interface ProviderProps {
+  children: ReactNode;
+}
+
+export const TransactionsProvider = ({ children }: ProviderProps) => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  const fetchTransactions = async () => {
+    const response = await transactionService.getTransactions();
+    setTransactions(response);
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  return (
+    <TransactionContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionContext.Provider>
+  );
+};
+
+export const useTransactions = () => useContext(TransactionContext);
