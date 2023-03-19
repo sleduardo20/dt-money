2becbe539c425fdbd538f53613a703bbc7873597
sleduardo20 <sl.eduardo20@gmail.com>
2023-03-19 01:23:30 -0300

    ♻ refactor formatter

--- src/presentation/utils/formatter.ts
+++ src/presentation/utils/formatter.ts
@@ -1,4 +1,8 @@
-export const dateFormatter = new Intl.DateTimeFormat('pt-BR');
+export const dateFormatter = (date: string) => {
+  const parsedDate = new Date(date);
+
+  return new Intl.DateTimeFormat('pt-BR').format(parsedDate);
+};
 export const priceFormatter = new Intl.NumberFormat('pt-BR', {
   style: 'currency',
   currency: 'BRL',
