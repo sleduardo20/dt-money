01a5ff968100010f93621f71fde7b8b061361b4a
sleduardo20 <sl.eduardo20@gmail.com>
2023-03-19 01:24:06 -0300

    ✨ add createTransactions

--- src/model/transactionGateway.ts
+++ src/model/transactionGateway.ts
@@ -1,5 +1,11 @@
 import { Transaction } from './transation';
 
-export interface TransactionGateway {
-  getTransactions: () => Promise<Transaction[]>;
+export interface TransactionsGateway {
+  getTransactions: (query?: string) => Promise<Transaction[]>;
+  createTransactions: (
+    description: string,
+    price: number,
+    category: string,
+    type: string,
+  ) => Promise<Transaction>;
 }
